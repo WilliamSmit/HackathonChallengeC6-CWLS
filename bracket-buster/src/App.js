9c096752eae57796d62805cf5e95187570325532
Scott Luxton <78744890+ScottLuxton@users.noreply.github.com>
2021-06-11 11:23:49 -0600

    SL

--- bracket-buster/src/App.js
+++ bracket-buster/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Description from './components/Description';
 import Navbar from './components/Navbar';
+import Flags from './components/Flags';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Description />
+      <Flags />
     </div>
   );
 }
